031fb0b92186523c7a46792b4fb2fd327c6c0c48
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2025-05-28 11:49:58 +0200

    typscript-todo: logging, configurable

--- typescript-todo/src/main.ts
+++ typescript-todo/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConsoleLogger, LogLevel } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      logLevels: (process.env.LOGGER_LEVELS ?? "log,warn,error,fatal").split(",") as LogLevel[],
+      json: (process.env.LOGGER_JSON ?? "false") == "true",
+    }),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
