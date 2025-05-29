2f9205adc078339a2f0d493835257eb09366da7d
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2025-05-29 18:52:24 +0200

    typscript-todo: request scope, middleware

--- typescript-todo/src/app/app.module.ts
+++ typescript-todo/src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { Logger, Module } from '@nestjs/common';
+import {
+  Logger,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  Scope,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Request, Response, NextFunction } from 'express';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    { provide: AppService, useClass: AppService, scope: Scope.REQUEST },
+  ],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   private readonly logger = new Logger(AppModule.name);
 
   constructor() {
     this.logger.verbose('Init');
+  }
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply((req: Request, res: Response, next: NextFunction) => {
+        this.logger.verbose(
+          `Request: ${req.url}, response: ${res.getHeaderNames().join(', ')}`,
+        );
+        next();
+      })
+      .forRoutes('*');
   }
 }
